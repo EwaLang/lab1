4470dc09690726df142f6827c661788a977d11cf
EwaLang <230990@student.pwr.edu.pl>
2016-04-01 10:23:43 +0200

    SOlution

--- main.cpp
+++ main.cpp
@@ -7,17 +7,45 @@ void drawCharSquare(char **square, int n);
 
 int main(int argc, char **argv)
 {
-	int n;
-	drawCharSquare(charSquare(n), n);	
+
+
+int n = atoi(argv[1]);
+int seed = atoi(argv[2]);
+srand(argc == 3 ? seed : time(NULL));
+drawCharSquare(charSquare(n), n);
 }
 
 char **charSquare(int n)
 {
-	char **square;
-	return square;
+char **square = new char * [n];
+int w = n; //Wysokosc
+int s = n; //Szerokosc
+
+for(int i = 0; i < n; i++)
+{
+square[i] = new char[n];
+}
+for(int i = 0 ; i < (w * s) ; i++)
+{
+square[i % s][i / s] = ( ( rand() % ( 'z' - 'a' ) ) + 'a' );
+}
+
+
+
+return square;
 }
 
 void drawCharSquare(char **square, int n)
 {
+int w = n;
+int s = n;
+for(int i = 0 ; i < (w * s) ; i++)
+{
+printf("%c", square[i % s][i / s]);
+if(i%s==n-1){
+	printf("\n");
+}
+}
+
 
 }
